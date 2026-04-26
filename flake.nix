f5a4f9abed0e782f248c179482fddceb35752a77
dprive <raphaelchouraqui92@gmail.com>
2026-04-26 11:46:47 +0200

    add zen-browser

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Home Manager configuration of raph";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
   outputs =
@@ -20,12 +20,12 @@
       homeConfigurations."raph" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+	
+	pecialArgs = {
+          inherit inputs;
+          zen-browser = inputs.zen-browser.packages."x86_64-linux".default;
+        };
       };
     };
 }
