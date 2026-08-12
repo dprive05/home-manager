d7a1585d92bae88bfa63889e406d7e18a3a48af5
dprive <raphaelchouraqui92@gmail.com>
2026-08-12 11:02:17 +0200

    build(packages[default]): remove lazygit

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -95,17 +95,11 @@ in
       default = false;
       description = "Enable the Zen Browser";
     };
-    lazygit= lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Enable the lazygit";
-    };
   };
 
   config = lib.mkIf cfg.enable {
     programs = { 
       zen-browser.enable = cfg.zen;
-      lazygit.enable = cfg.lazygit;
     };
     home.packages =
       with pkgs;
