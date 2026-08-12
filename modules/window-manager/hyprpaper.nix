e233f965514db9e3db4ed2c0d457562a0ec8778e
dprive <raphaelchouraqui92@gmail.com>
2026-08-12 21:25:51 +0200

    feat(window-manager[hyprpaper]): fetch the wallpaper

--- modules/window-manager/hyprpaper.nix
+++ modules/window-manager/hyprpaper.nix
@@ -7,6 +7,11 @@
 }:
 let
   cfg = config.window-manager.hyprpaper;
+  wallpaper = pkgs.fetchurl {
+    url = "https://github.com/dprive05/home-manager/blob/main/modules/window-manager/wallpaper/wallpaper3.jpg?raw=true";
+    sha256 = "0s29iar5cz9ajya1cw1b7jc0zq7m7k8js6qmj6ihj7iga6zr69id";
+  };
+
 in
 {
   config = lib.mkIf cfg {
@@ -14,18 +19,19 @@ in
       hyprpaper
     ];
 
+    home.file."Pictures/wallpaper.png".source = wallpaper;
     services.hyprpaper = {
       enable = true;
 
       settings = {
         splash = false;
 
-        preload = [ ".config/home-manager/modules/window-manager/wallpaper/wallpaper3.jpg" ];
+        preload = [ "/home/${config.home.username}/Pictures/wallpaper.png" ];
 
         wallpaper = [
           {
             monitor = "";
-            path = ".config/home-manager/modules/window-manager/wallpaper/wallpaper3.jpg";
+            path = "/home/${config.home.username}/Pictures/wallpaper.png" ;
           }
         ];
       };
